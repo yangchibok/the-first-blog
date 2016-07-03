b4f66c6bc77e292400e05c664903c4222c7f4372
Yangchibok <yangchibok@gmail.com>
2016-07-04 00:33:17 +0900

    added views to create/edit blog post inside the sit.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
@@ -9,6 +10,17 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.putlished_date = timezone.now()
+            post.save()
+            return redirect(request, 'blog/post_edit.html', {'form':form})
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
 
 # Create your views here.
